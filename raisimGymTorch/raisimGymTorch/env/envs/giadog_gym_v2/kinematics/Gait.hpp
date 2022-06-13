9c30d3c03423e0c20ef7c7f98dd35889bdb3a052
Amin Arriaga <aminlorenzo.14@gmail.com>
2022-06-13 16:05:00 -0400

    Delete .cpp files

--- raisimGymTorch/raisimGymTorch/env/envs/giadog_gym_v2/kinematics/Gait.hpp
+++ raisimGymTorch/raisimGymTorch/env/envs/giadog_gym_v2/kinematics/Gait.hpp
@@ -27,7 +27,12 @@ class BaseGait {
         Eigen::Vector4d sigma_0;
 
         BaseGait(void) { }
-        BaseGait(const Eigen::Vector4d sigma_0, double f0, const Eigen::Vector3d hz);
+        BaseGait(const Eigen::Vector4d sigma_0, double f0, const Eigen::Vector3d hz)
+        {
+            this->sigma_0 = sigma_0;
+            this->f0 = f0;
+            this->hz = hz;
+        }
         
         /*
             Generates a vector in R^3 representing the desired foot position
@@ -49,7 +54,25 @@ class BaseGait {
             double sigma_i_0,
             double t,
             double f_i
-        );
+        ) {
+            Eigen::Vector3d position;
+            double sigma_i, k, h;
+            bool condition_1, condition_2;
+
+            sigma_i = std::fmod(sigma_i_0 + t * (this->f0 + f_i), (2 * M_PI));
+            k       = 2 * (sigma_i - M_PI) / M_PI;
+            h       = 0.8 * H;
+
+            condition_1 = (k <= 1 && k >= 0);
+            condition_2 = (k >= 1 && k <= 2);
+
+            position += h * (-2 * k * k * k + 3 * k * k)  * this->hz * condition_1;
+            position += h * (2 * k * k * k - 9 * k * k + 12 * k - 4) * 
+                this->hz * condition_2;
+
+            // See that if no condition is meet the position is the vector 0
+            return {position, sigma_i}; 
+        }
 
         /*
             Compute the foot trajectories for the given frequencies, for all
@@ -64,7 +87,22 @@ class BaseGait {
                     Vector of the four frequencies offsets of the four legs.
         */
         std::tuple<Eigen::MatrixXd, Eigen::Vector4d, Eigen::MatrixXd> 
-        compute_foot_trajectories(float t, Eigen::Vector4d frequencies);
+        compute_foot_trajectories(float t, Eigen::Vector4d frequencies)
+        {
+            Eigen::Vector4d FTG_frequencies;
+            Eigen::MatrixXd FTG_phases(4, 2);
+            Eigen::MatrixXd target_foot_positions(4, 3);
+
+            for (int i = 0; i < 4; i++) {
+                auto [r, sigma_i] = this->FTG(this->sigma_0[i], t, frequencies[i]);
+                FTG_frequencies[i] = this->f0 + frequencies[i];
+                FTG_phases(i, 0)   = std::sin(sigma_i);
+                FTG_phases(i, 1)   = std::cos(sigma_i);
+                target_foot_positions.row(i) = r;
+            }
+
+            return {target_foot_positions, FTG_frequencies, FTG_phases};
+        }
 };
 
 /*
@@ -107,5 +145,34 @@ Eigen::MatrixXd direction_deltas(
     int turn_dir,
     bool add_cartesian_delta,
     bool add_angular_delta
-);
+) {
+    Eigen::MatrixXd delta(4, 3);
+
+    for (int i = 0; i < 4; i++) {
+        Eigen::Vector3d position_delta(0, 0, 0);
+        
+        // Position delta
+        position_delta(0) = 1.7 * cos(command_dir) * delta_t * ftg_sine_phases(i) * 
+            ftg_freqs(i) * LEG_SPAN;
+        position_delta(1) = 1.02 * sin(command_dir) * delta_t * ftg_sine_phases(i) *
+            ftg_freqs(i) * LEG_SPAN;
+
+        // Rotation delta (Look Mom no branching!!)
+        Eigen::Vector3d rotation_delta(0, 0, 0);
+        
+        double theta = M_PI/4;
+        double phi_arc = (i == 0) * -theta + (i == 1) * -(M_PI - theta) + 
+            (i == 2) *  theta + (i == 3) * (M_PI - theta);
+        
+        rotation_delta(0) = 0.68 * -cos(phi_arc) * delta_t * ftg_sine_phases(i) * 
+            turn_dir * ftg_freqs(i) * LEG_SPAN;
+        rotation_delta(1) = 0.68 * -sin(phi_arc) * delta_t * ftg_sine_phases(i) * 
+            turn_dir * ftg_freqs(i) * LEG_SPAN;
+        
+        delta.row(i) =  (position_delta * add_cartesian_delta + \
+                         rotation_delta * add_cartesian_delta);
+        }
+
+    return delta;
+}
 
--- raisimGymTorch/raisimGymTorch/env/envs/giadog_gym_v2/kinematics/TransfMatrices.hpp
+++ raisimGymTorch/raisimGymTorch/env/envs/giadog_gym_v2/kinematics/TransfMatrices.hpp
@@ -33,7 +33,18 @@
         rotationMatrix: Eigen::Matrix3d
             Rotation matrix.
 */
-Eigen::Matrix3d rotation_matrix_from_euler(double roll, double pitch, double yaw);
+Eigen::Matrix3d rotation_matrix_from_euler(double roll, double pitch, double yaw)
+{
+    Eigen::AngleAxisd rollAngle(roll, Eigen::Vector3d::UnitZ());
+    Eigen::AngleAxisd yawAngle(yaw, Eigen::Vector3d::UnitY());
+    Eigen::AngleAxisd pitchAngle(pitch, Eigen::Vector3d::UnitX());
+
+    Eigen::Quaternion<double> q = rollAngle * yawAngle * pitchAngle;
+
+    Eigen::Matrix3d rotationMatrix = q.matrix();
+    
+    return rotationMatrix;
+}
 
 /*
     Returns the transformation matrices from the hip to the leg base.
@@ -50,4 +61,33 @@ Eigen::Matrix3d rotation_matrix_from_euler(double roll, double pitch, double yaw
             the leg base, for each of the robots legs.
         The order of the matrices is: LF, RF, LB, RB.
 */
-std::vector<Eigen::Matrix4d> transf_matrices(Eigen::Vector3d base_rpy);
+std::vector<Eigen::Matrix4d> transf_matrices(Eigen::Vector3d base_rpy)
+{
+    std::vector<Eigen::Matrix4d> T_matrices;
+    
+    // We transform the base orientation from quaternion to matrix
+    // We also get the base euler angles
+    
+    Eigen::Matrix3d R = rotation_matrix_from_euler(base_rpy(0), base_rpy(1), 0);
+        
+    for (int i = 0; i < 4; i++) {
+        // We calculate the Hi frame position relative to the leg base position 
+        // (hip)
+        Eigen::Vector3d  p(0, H_OFF * pow(-1, i), -LEG_SPAN);
+        
+        // Finally we concatenate the rotation matrix and position vector
+        // To get the transformation matrix of the Hi horizontal frame expressed
+        // in the leg base frame
+        Eigen::Matrix4d T_i;
+        T_i <<
+            R.coeff(0,0), R.coeff(0,1), R.coeff(0,2), p.coeff(0),
+            R.coeff(1,0), R.coeff(1,1), R.coeff(1,2), p.coeff(1),
+            R.coeff(2,0), R.coeff(2,1), R.coeff(2,2), p.coeff(2),
+            0           , 0           , 0           , 1         
+        ;
+
+        T_matrices.push_back(T_i);
+    }
+    
+    return T_matrices;
+}
